993e8129c8dd71ec3372d674051e9ec6c0288ad1
Pilar Perez <josepilar21@gmail.com>
2019-05-03 14:40:41 -0700

    Addes some css and test html

--- src/App.js
+++ src/App.js
@@ -1,15 +1,71 @@
 import React from 'react';
 import './App.css';
-import { Button } from 'antd';
+import { Layout, Menu } from 'antd';
 
 function App() {
-  const perro = () => {
-    console.log('you just clicked me perro');
-  }
   return (
-    <div className="App">
-      <Button type="primary" onClick={perro}>PERRO</Button>
-    </div>
+    <Layout className='Container'>
+      <Layout.Header className='header'>
+        <div className="logo" />
+        TEST
+      </Layout.Header>
+      <Layout>
+        <Layout.Sider breakpoint="lg" collapsedWidth="0" width={200}>
+          <Menu mode='inline' className='Side'>
+            <Menu.SubMenu title='tests'>
+              <Menu.Item>Test1</Menu.Item>
+              <Menu.Item>Test2</Menu.Item>
+              <Menu.Item>Test3</Menu.Item>
+              <Menu.Item>Test4</Menu.Item>
+            </Menu.SubMenu>
+            <Menu.SubMenu title='perros'>
+            <Menu.Item>Perro1</Menu.Item>
+              <Menu.Item>Perro2</Menu.Item>
+              <Menu.Item>Perro3</Menu.Item>
+              <Menu.Item>Perro2</Menu.Item>
+              <Menu.Item>Perro3</Menu.Item>
+              <Menu.Item>Perro2</Menu.Item>
+              <Menu.Item>Perro3</Menu.Item>
+              <Menu.Item>Perro2</Menu.Item>
+              <Menu.Item>Perro3</Menu.Item>
+              <Menu.Item>Perro2</Menu.Item>
+              <Menu.Item>Perro3</Menu.Item>
+              <Menu.Item>Perro2</Menu.Item>
+              <Menu.Item>Perro3</Menu.Item>
+              <Menu.Item>Perro2</Menu.Item>
+              <Menu.Item>Perro3</Menu.Item>
+              <Menu.Item>Perro2</Menu.Item>
+              <Menu.Item>Perro3</Menu.Item>
+              <Menu.Item>Perro2</Menu.Item>
+              <Menu.Item>Perro3</Menu.Item>
+              <Menu.Item>Perro2</Menu.Item>
+              <Menu.Item>Perro3</Menu.Item>
+              <Menu.Item>Perro2</Menu.Item>
+              <Menu.Item>Perro3</Menu.Item>
+              <Menu.Item>Perro2</Menu.Item>
+              <Menu.Item>Perro3</Menu.Item>
+              <Menu.Item>Perro2</Menu.Item>
+              <Menu.Item>Perro3</Menu.Item>
+              <Menu.Item>Perro2</Menu.Item>
+              <Menu.Item>Perro3</Menu.Item>
+              <Menu.Item>Perro2</Menu.Item>
+              <Menu.Item>Perro3</Menu.Item>
+              <Menu.Item>Perro2</Menu.Item>
+              <Menu.Item>Perro3</Menu.Item>
+              <Menu.Item>Perro2</Menu.Item>
+              <Menu.Item>Perro3</Menu.Item>
+              <Menu.Item>Perro2</Menu.Item>
+              <Menu.Item>Perro3</Menu.Item>
+              <Menu.Item>Perro2</Menu.Item>
+              <Menu.Item>Perro3</Menu.Item>
+            </Menu.SubMenu>
+          </Menu>
+        </Layout.Sider>
+        <Layout>
+          <Layout.Content className='MainContent'>Here goes the dinamic content heuheu</Layout.Content>
+        </Layout>
+      </Layout>
+    </Layout>
   );
 }
 
