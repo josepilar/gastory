6b9a235728b26ee7c5ef6187444bed941291664d
Pilar Perez <josepilar21@gmail.com>
2019-07-11 02:43:10 -0600

    Adding basic configuration for google spreadsheets

--- src/App.js
+++ src/App.js
@@ -1,8 +1,50 @@
-import React from 'react';
+/* global gapi */
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import { Layout, Menu } from 'antd';
+import GoogleLogin from 'react-google-login';
+import axios from 'axios';
+
+const documentKey = '1N8JjCUfGQHH7RULfxJdDh6_20JmJRaR0hInATBttKso';
+const clientId = '397035199840-5fnvhn7iaakgnhnv99h2hc1sis6aan6p.apps.googleusercontent.com';
 
 function App() {
+
+  const [user, setUser] = useState();
+
+  const responseGoogle = async (user) => {
+    setUser(user);
+    localStorage.setItem('gastoryUser', JSON.stringify(user));
+  };
+
+  useEffect(() => {
+    const localUser = localStorage.getItem('gastoryUser');
+    if (localUser) {
+      setUser(JSON.parse(localUser));
+    }
+  }, []);
+
+  const loadData = async () => {
+    const resp = await axios({
+      method: 'post',
+      url: `https://sheets.googleapis.com/v4/spreadsheets/${documentKey}/values:batchGetByDataFilter`,
+      headers: {
+        Authorization: `${user.tokenObj.token_type} ${user.tokenObj.access_token}`
+      },
+      data: {
+        dataFilters: [
+          {
+            gridRange: {
+              sheetId: 0,
+              startRowIndex: 1
+            }
+          }
+        ]
+      }
+    });
+    console.log(resp);
+  }
+
   return (
     <Layout className='Container'>
       <Layout.Header className='header'>
@@ -62,7 +104,18 @@ function App() {
           </Menu>
         </Layout.Sider>
         <Layout>
-          <Layout.Content className='MainContent'>Here goes the dinamic content heuheu</Layout.Content>
+          <Layout.Content className='MainContent'>
+            {!user && <GoogleLogin
+              clientId={clientId}
+              buttonText="Login"
+              onSuccess={responseGoogle}
+              onFailure={responseGoogle}
+              discoveryDocs="https://sheets.googleapis.com/$discovery/rest?version=v4"
+              scope="https://www.googleapis.com/auth/spreadsheets"
+            />}
+            <br/>
+            <button style={{marginLeft: 'auto', marginRight: 'auto', position: 'abosulute'}} onClick={loadData}>load data</button>
+          </Layout.Content>
         </Layout>
       </Layout>
     </Layout>
