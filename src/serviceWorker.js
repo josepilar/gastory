dae58abc0a24da542fca7214666765496d2faf71
Pilar Perez <josepilar21@gmail.com>
2020-04-26 00:45:31 -0600

    enabling service worker

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -21,17 +21,13 @@ const isLocalhost = Boolean(
 );
 
 export function register(config) {
-  console.log(`running in ${process.env.NODE_ENV}`);
-
-  // if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
-  if ('serviceWorker' in navigator) {
+  if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
     // The URL constructor is available in all browsers that support SW.
     const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href);
     if (publicUrl.origin !== window.location.origin) {
       // Our service worker won't work if PUBLIC_URL is on a different origin
       // from what our page is served on. This might happen if a CDN is used to
       // serve assets; see https://github.com/facebook/create-react-app/issues/2374
-      console.log('Perro SW');
       return;
     }
 
